c831cc643c4167e7640d10cc7f6eca3fb1bd1387
Stefan Haertel <haertel@studserv.uni-leipzig.de>
2011-11-02 21:48:07 +0100

    made the background rect size depend on the chart size

--- pages/playground.js
+++ pages/playground.js
@@ -1,5 +1,8 @@
 var data = [0, 0, 0, 0, 30, 50, 60, 60, 30, 80, 90, 90, 40, 100, 70, 70, 100, 80, 70, 50, 50, 20, 0, 0, 20, 50, 60, 70, 100, 80]
 
+const chartWidth = 800;
+const chartHeight= 600;
+
 function setUp(){
 	d3.select("body")
 		.style("background-color", d3.rgb(20, 20, 20));
@@ -7,13 +10,13 @@ function setUp(){
 	var svg = d3.select("#chart")
 		.append("svg:svg")
 			.attr("class", "chart")
-			.attr("width", 800)
-			.attr("height", 800);
+			.attr("width", chartWidth)
+			.attr("height", chartHeight);
 	
 	svg.append("svg:rect")
 		.attr("fill", d3.rgb(100, 100, 100))
-		.attr("width", 400)
-		.attr("height", 400);
+		.attr("width", svg.attr("width"))
+		.attr("height", svg.attr("height"));
 	
 	svg.append("svg:text")
 		.attr("text-anchor", "middle")
@@ -21,7 +24,5 @@ function setUp(){
 		.attr("x", 100)
 		.attr("y", 100)
 		.text("test");
-	
-
 }
 // colors: 103, 0, 31; 178, 24, 43; 214, 96, 77; 244, 165, 130; 253, 219, 199; 224, 224, 224; 186, 186, 186; 135, 135, 135; 77, 77, 77; 26, 26, 26; 
