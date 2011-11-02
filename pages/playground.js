e35e4aa9023c79bfcf2b402ebae52b79d96e1c07
Stefan Haertel <haertel@studserv.uni-leipzig.de>
2011-11-02 21:40:58 +0100

    added a rect and some text

--- pages/playground.js
+++ pages/playground.js
@@ -3,5 +3,25 @@ var data = [0, 0, 0, 0, 30, 50, 60, 60, 30, 80, 90, 90, 40, 100, 70, 70, 100, 80
 function setUp(){
 	d3.select("body")
 		.style("background-color", d3.rgb(20, 20, 20));
+
+	var svg = d3.select("#chart")
+		.append("svg:svg")
+			.attr("class", "chart")
+			.attr("width", 800)
+			.attr("height", 800);
+	
+	svg.append("svg:rect")
+		.attr("fill", d3.rgb(100, 100, 100))
+		.attr("width", 400)
+		.attr("height", 400);
+	
+	svg.append("svg:text")
+		.attr("text-anchor", "middle")
+		.attr("fill", d3.rgb(255, 255, 255))
+		.attr("x", 100)
+		.attr("y", 100)
+		.text("test");
+	
+
 }
 // colors: 103, 0, 31; 178, 24, 43; 214, 96, 77; 244, 165, 130; 253, 219, 199; 224, 224, 224; 186, 186, 186; 135, 135, 135; 77, 77, 77; 26, 26, 26; 
