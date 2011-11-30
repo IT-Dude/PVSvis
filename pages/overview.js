0b8d4d63b9f29fae90f37aeea7579b8014471191
Stefan Haertel <haertel@studserv.uni-leipzig.de>
2011-11-30 04:27:21 +0100

    added some random data generator function, does not seem to work correctly

--- pages/overview.js
+++ pages/overview.js
@@ -1,14 +1,29 @@
 const months = 5;
 const days = 15;
+const randomRange = 0.1;
 
-var dataSet;
+var dataSet = new Array(months);
+generateSampleData();
 
 function generateSampleData(){
 	for(var i = 0; i < months; i++){
+		var randomPivot = Math.random();
+		var monthDataSet = new Array(days);
+		
 		for(var j = 0; j < days; j++){
+			var min = randomPivot - randomRange;
+			var max = randomPivot + randomRange;
 			
+			var value = min + (Math.random() * (max - min));
+			value *= 100;
+			
+			monthDataSet.push(value);			
 		}
+		
+		dataSet[i] = monthDataSet;
 	}
+	
+	console.log(dataSet);
 }
 
 function setUpOverview(){
